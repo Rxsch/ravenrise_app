c8bb5c0c4a03fe8e7f8ab5b4b297c042ca400cda
Rxsch <danielrangosch2004@gmail.com>
2025-12-16 22:56:45 -0500

    Testing the fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>The number 1</Text>
+      <Text>Alarm App Ola que ace</Text>
     </View>
   )
 }
-
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
