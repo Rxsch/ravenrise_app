60854240ea1266786702a6e0bf4ca1c47718d216
Rxsch <danielrangosch2004@gmail.com>
2025-12-16 23:40:39 -0500

    Enter images

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image} from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
   return (
     <View style={styles.container}>
+      <Image source={Logo} /> 
       <Text style={styles.title}>The number 1</Text>
-      <Text>Alarm App Ola que ace</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30}}>Alarm App Ola que ace</Text>
+
+     <View style={styles.card}> 
+        <Text> Hello, this is a card</Text>
+     </View> 
+
     </View>
   )
 }
@@ -19,5 +27,11 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: 'bold',
         fontSize: 18
+    },
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)'
     }
 })
