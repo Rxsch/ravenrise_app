9f801000e2b7b9aa1adfdde211f441f9e83067c2
Rxsch <danielrangosch2004@gmail.com>
2025-12-24 01:44:13 -0500

    Added Contact Page and Created menu

---
+++ app/contact.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router'
+
+const Contact = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contact Page</Text>
+
+      <Link href="/" style={styles.link}> Back home</Link>
+    </View>
+  )
+}
+
+export default About
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    img: {
+        height: 150,
+        width: 150,
+        marginVertical: 20,
+    },
+    link:{
+        marginVertical: 10,
+        borderBottomWidth: 1
+    },
+})
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,17 @@
 import { StyleSheet, Text, View, Image} from 'react-native'
-import Logo from '../assets/img/logo_light.png'
+import Logo from '../assets/logo_light.png'
+import {Link} from 'expo-router'
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Image source={Logo} /> 
+      <Image source={Logo} style={styles.img} />  
       <Text style={styles.title}>The number 1</Text>
 
       <Text style={{ marginTop: 10, marginBottom: 30}}>Alarm App Ola que ace</Text>
 
-     <View style={styles.card}> 
-        <Text> Hello, this is a card</Text>
-     </View> 
-
+     <Link href="/about" style={styles.link}> About Page</Link>
+      <Link href="/contact" style={styles.link}> Contact Page</Link>
     </View>
   )
 }
@@ -28,10 +27,21 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 18
     },
+    img: {
+        height: 150,
+        width: 150,
+        marginVertical: 20,
+
+    },
+    link:{
+        marginVertical: 10,
+        borderBottomWidth: 1
+    },
+    /*
     card: {
         backgroundColor: '#eee',
         padding: 20,
         borderRadius: 5,
         boxShadow: '4px 4px rgba(0,0,0,0.1)'
-    }
+    }*/
 })
